625335cc1025297df793da9fc263683d84c4fae5
TKuzas-R7 <tkuzas@rapid7.com>
2022-09-30 13:17:48 +0100

    formatted file and fixed bug that only returned spells that were class specific

--- script.py
+++ script.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-# Bugs include: Select sorcerer == crash
-# This is so stupid but it helped kill an hour 
+# This is so stupid but, it helped kill an hour
 import json
 import random
 
-class spellClass:
+
+class SpellClass:
     bard = 'Bard'
     cleric = 'Cleric'
     druid = 'Druid'
@@ -14,40 +14,40 @@ class spellClass:
     warlock = 'Warlock'
     wizard = 'Wizard'
 
-class color:
-   PURPLE = '\033[95m'
-   CYAN = '\033[96m'
-   DARKCYAN = '\033[36m'
-   BLUE = '\033[94m'
-   GREEN = '\033[92m'
-   YELLOW = '\033[93m'
-   RED = '\033[91m'
-   BOLD = '\033[1m'
-   UNDERLINE = '\033[4m'
-   END = '\033[0m'
-   
 
-  
+class Color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+
 # Input: class name
 # Process: Filter json by class (user input)
 # Select random spell from new list
 # Output: spell name
-def chooseSpell(className):
+def choose_spell(class_name):
     # Load the data from the local spells.json file
     with open('spells.json') as f:
         data = json.load(f)
         # Filter the data by class
-        filteredData = [spell for spell in data if spell['class'] == className]
+        filteredData = [spell for spell in data if class_name in spell['class']]
         # Select random spell
         randomSpell = filteredData[random.randint(0, len(filteredData) - 1)]
         # Print random spell
         return randomSpell['name']
 
 
-# Print out list of all spellClass variables
+# Print out list of all SpellClass variables
 # Then take user input for class
 # Return class name
-def chooseClass():
+def choose_class():
     print("Bard")
     print("Cleric")
     print("Druid")
@@ -57,24 +57,25 @@ def chooseClass():
     print("Warlock")
     print("Wizard")
     print()
-    className = input("\n" + color.BOLD + color.UNDERLINE + "Choose a class: " + color.END)
+    class_name = input("\n" + Color.BOLD + Color.UNDERLINE + "Choose a class: " + Color.END)
     # Error handling
-    while className not in spellClass.__dict__.values():
-        className = input("\n" + color.BOLD + color.UNDERLINE + "Choose a class: " + color.END)
+    while class_name not in SpellClass.__dict__.values():
+        class_name = input("\n" + Color.BOLD + Color.UNDERLINE + "Choose a class: " + Color.END)
 
     # Return class name
-    return className
+    return class_name
+
 
 if __name__ == '__main__':
 
     status = True
 
     # Main Loop
-    while status == True:
+    while status is True:
         # Print random entry
-        print('\n' + color.BOLD + chooseSpell(chooseClass()) + color.END + '\n')
+        print('\n' + Color.BOLD + choose_spell(choose_class()) + Color.END + '\n')
         # Continue loop or break
-        v = input("Another? [" + color.GREEN + 'y' + color.END + '/' + color.RED + 'n' + color.END + ']').lower()
+        v = input("Another? [" + Color.GREEN + 'y' + Color.END + '/' + Color.RED + 'n' + Color.END + ']').lower()
         if v == 'n':
             break
         if v == 'y':
